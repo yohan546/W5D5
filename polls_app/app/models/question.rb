d4bc87dccb3d473b940dbf95cc1fe7c8ff6fccad
Kevin <kevinmao.xy@gmail.com>
2021-01-29 17:59:27 -0500

    bug fix on question model

--- polls_app/app/models/question.rb
+++ polls_app/app/models/question.rb
@@ -11,11 +11,11 @@
 class Question < ApplicationRecord
     has_many :answer_choices,
         primary_key: :id,
-        foriegn_key: :question_id,
+        foreign_key: :question_id,
         class_name: 'AnswerChoice'
 
     belongs_to :poll,
         primary_key: :id,
-        foriegn_key: :poll_id,
+        foreign_key: :poll_id,
         class_name: 'Poll'
 end
